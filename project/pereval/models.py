eaa0a94d430e7eadc965fc117d92721170fccf2b
stanislav <stasiklim12@gmail.com>
2024-05-08 15:59:40 +0700

    Внес изменение в модель

--- project/pereval/models.py
+++ project/pereval/models.py
@@ -21,28 +21,33 @@ class Level(models.Model):
     spring = models.TextField(verbose_name='Весна', null=True, blank=True)
 
 class Images(models.Model):
-    img = models.ImageField(max_length=2000, verbose_name='Изображение', null=True, blank=True)
+    data = models.ImageField(max_length=2000, verbose_name='Изображение', null=True, blank=True)
     title = models.TextField(max_length=255, null=True, blank=True)
     date_added = models.DateField(auto_now_add=True)
 
 
-
 class Pereval(models.Model):
-    STATUS_CHOICES = [
-        ("new", "новый"),
-        ("pending", "в работе"),
-        ("accepted", "принят"),
-        ("rejected", "отклонен"),
-    ]
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    coords = models.OneToOneField(Coords, on_delete=models.CASCADE)
-    level = models.ForeignKey(Level, on_delete=models.CASCADE)
-    status = models.TextField(choices=STATUS_CHOICES)
-    beauty_title = models.TextField(blank=True,verbose_name='Основное название вершины', null=True)
-    title = models.TextField(blank=True, null=True, verbose_name='Название вершины')
-    other_titles = models.TextField(blank=True, null=True,verbose_name='Другое название' )
-    connect = models.TextField(blank=True,verbose_name='Связывает', null=True)
-    add_time = models.TimeField(auto_now_add=True, blank=True, null=True)
+    NEW = 'NW'
+    PENDING = 'PN'
+    ACCEPTED = 'AC'
+    REJECTED = 'RJ'
+    STATUS_CHOICES = (
+        ("NW", "new"),
+        ("PN", "pending"),
+        ("AC", "accepted"),
+        ("RJ", "rejected"),
+)
+
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    image_id = models.ManyToManyField(Images, through='PerevalImages')
+    coords_id = models.OneToOneField(Coords, on_delete=models.CASCADE)
+    level_id = models.OneToOneField(Level, on_delete=models.CASCADE)
+    status = models.CharField(choices=STATUS_CHOICES, default='NEW')
+    beauty_title = models.CharField(max_length=128)
+    title = models.CharField(max_length=128)
+    other_titles = models.CharField(max_length=128)
+    connect = models.CharField(max_length=128)
+    add_time = models.DateTimeField(auto_now_add=True)
 
 class PerevalImages(models.Model):
     image = models.ForeignKey(Images, on_delete=models.CASCADE)
