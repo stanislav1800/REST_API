476b7152feb3b7972f43fe6cccec65883af6e587
stanislav <stasiklim12@gmail.com>
2024-05-08 16:51:48 +0700

    Внес изменения

--- project/pereval/views.py
+++ project/pereval/views.py
@@ -1,3 +1,32 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework import permissions
 
-# Create your views here.
+from .serializers import *
+from .models import *
+
+
+class UserViewset(viewsets.ModelViewSet):
+   queryset = User.objects.all()
+   serializer_class = UsersSerializer
+
+class CoordsViewSet(viewsets.ModelViewSet):
+   queryset = Coords.objects.all()
+   serializer_class = CoordSerializer
+
+
+class LevelViewSet(viewsets.ModelViewSet):
+    queryset = Coords.objects.all()
+    serializer_class = LevelSerializer
+
+
+class ImageViewSet(viewsets.ModelViewSet):
+    queryset = Images.objects.all()
+    serializer_class = ImagesSerializer
+
+
+class PerevalViewset(viewsets.ModelViewSet):
+   queryset = Pereval.objects.all()
+   serializer_class = PerevalSerializer
+
+
