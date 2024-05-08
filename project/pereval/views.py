2311139e016513bfeefd5589707649cf544bd9d9
stanislav <stasiklim12@gmail.com>
2024-05-08 17:04:22 +0700

    Определил permission_classes

--- project/pereval/views.py
+++ project/pereval/views.py
@@ -9,20 +9,24 @@ from .models import *
 class UserViewset(viewsets.ModelViewSet):
    queryset = User.objects.all()
    serializer_class = UsersSerializer
+   permission_classes = [permissions.IsAuthenticated]
 
 class CoordsViewSet(viewsets.ModelViewSet):
    queryset = Coords.objects.all()
    serializer_class = CoordSerializer
+   permission_classes = [permissions.IsAuthenticated]
 
 
 class LevelViewSet(viewsets.ModelViewSet):
     queryset = Coords.objects.all()
     serializer_class = LevelSerializer
+    permission_classes = [permissions.IsAuthenticated]
 
 
 class ImageViewSet(viewsets.ModelViewSet):
     queryset = Images.objects.all()
     serializer_class = ImagesSerializer
+    permission_classes = [permissions.IsAuthenticated]
 
 
 class PerevalViewset(viewsets.ModelViewSet):
