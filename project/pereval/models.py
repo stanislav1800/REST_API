e73cf3eba8ab2eaf3e4dab1d9ecc4dc77a6f6b5d
stanislav <stasiklim12@gmail.com>
2024-05-08 15:31:31 +0700

    Создал модели для БД

--- project/pereval/models.py
+++ project/pereval/models.py
@@ -1,3 +1,50 @@
 from django.db import models
 
-# Create your models here.
+
+
+class User(models.Model):
+    email = models.EmailField()
+    phone_nombers = models.TextField(max_length=255, verbose_name='Контактный телефон')
+    fam = models.TextField(max_length=255,verbose_name='Фамилия')
+    name = models.TextField(max_length=255, verbose_name='Имя')
+    otc = models.TextField(max_length=255, verbose_name='Отчество')
+
+class Coords(models.Model):
+    latitude = models.FloatField(verbose_name='Широта')
+    longitude = models.FloatField(verbose_name='Долгота')
+    height = models.IntegerField(verbose_name='Высота')
+
+class Level(models.Model):
+    winter = models.TextField(verbose_name='Зима', null=True, blank=True)
+    summer = models.TextField(verbose_name='Лето', null=True, blank=True)
+    autumn = models.TextField(verbose_name='Осень', null=True, blank=True)
+    spring = models.TextField(verbose_name='Весна', null=True, blank=True)
+
+class Images(models.Model):
+    img = models.ImageField(max_length=2000, verbose_name='Изображение', null=True, blank=True)
+    title = models.TextField(max_length=255, null=True, blank=True)
+    date_added = models.DateField(auto_now_add=True)
+
+
+
+class Pereval(models.Model):
+    STATUS_CHOICES = [
+        ("new", "новый"),
+        ("pending", "в работе"),
+        ("accepted", "принят"),
+        ("rejected", "отклонен"),
+    ]
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    coords = models.OneToOneField(Coords, on_delete=models.CASCADE)
+    level = models.ForeignKey(Level, on_delete=models.CASCADE)
+    status = models.TextField(choices=STATUS_CHOICES)
+    beauty_title = models.TextField(blank=True,verbose_name='Основное название вершины', null=True)
+    title = models.TextField(blank=True, null=True, verbose_name='Название вершины')
+    other_titles = models.TextField(blank=True, null=True,verbose_name='Другое название' )
+    connect = models.TextField(blank=True,verbose_name='Связывает', null=True)
+    add_time = models.TimeField(auto_now_add=True, blank=True, null=True)
+
+class PerevalImages(models.Model):
+    image = models.ForeignKey(Images, on_delete=models.CASCADE)
+    pereval = models.ForeignKey(Pereval, on_delete=models.CASCADE)
+
--- project/project/settings.py
+++ project/project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import psycopg2
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -75,11 +76,22 @@ WSGI_APPLICATION = 'project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    },
 }
 
 
